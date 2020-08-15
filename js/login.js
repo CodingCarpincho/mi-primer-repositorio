8f03cc05c15a2f12510da05df20da5bd0ee9b3d8
codingcarpincho <nekosenshi1@gmail.com>
2020-08-15 20:23:44 -0300

    2

--- js/login.js
+++ js/login.js
@@ -22,6 +22,6 @@ function onSignIn(googleUser) {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
-      location.href="index.html"
+      location.href="index.html";
     });
   }
