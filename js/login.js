9ec8a6d21d20dcaba2bde1133493a896cb5c7326
codingcarpincho <nekosenshi1@gmail.com>
2020-11-29 19:57:22 -0300

    "agregado de nuevo el sing in de google"

--- js/login.js
+++ js/login.js
@@ -1,6 +1,3 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 
 function saveLogin(user, pass) {
     if(user.trim() === ""|| pass.trim() === "") {
@@ -8,15 +5,26 @@ function saveLogin(user, pass) {
     } else {
       localStorage.setItem("user", user.trim());
       localStorage.setItem("pass", pass.trim());
-      location.href="login.html"
+      location.href="login.html";
     }
 }
 
-/*function onSignIn(googleUser) {
+function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    location.href="login.html"
-}*/
+    location.href="login.html";
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      document.location.href = "index.html";
+    });
+  }
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
 
 document.addEventListener("DOMContentLoaded", function(e){
-}
-);
+});
